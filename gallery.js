0390f7c233db5142d77942bce88623a9f2024962
Vedika-Agrawal <vedikaagrawal53040@gmail.com>
2024-01-05 21:15:46 +0530

    funtionality to download and delete btn

--- gallery.js
+++ gallery.js
@@ -25,6 +25,14 @@ setTimeout(()=>{
                 `;
 
                 galleryCont.appendChild(mediaElem);
+
+
+
+                let deleteBtn = mediaElem.querySelector(".delete");
+                deleteBtn.addEventListener("click", deleteListener);
+                let downloadBtn = mediaElem.querySelector(".download");
+                downloadBtn.addEventListener("click",dowloadListener);
+
             })
         }
 
@@ -53,14 +61,76 @@ setTimeout(()=>{
             `;
 
             galleryCont.appendChild(mediaElem);
+
+
+            // Listners 
+            let deleteBtn = mediaElem.querySelector(".delete");
+            deleteBtn.addEventListener("click", deleteListener);
+            let downloadBtn = mediaElem.querySelector(".download");
+            downloadBtn.addEventListener("click",dowloadListener);
         })
     }
 
-
-      
-    }
+}
    
 },1000)
+
+// remove ui and database ir idexed db 
+
+function deleteListener(e){
+
+    // db removal 
+    let id = e.target.parentElement.getAttribute("id");
+    let type = id.slice(0,3);
+    if(type ==="vid"){
+        let videoTransaction = db.transaction("video","readwrite");
+        let videoStore = videoTransaction.objectStore("video");
+        videoStore.delete(id);
+    }
+    else if (type ==="img"){
+        let imgTransaction = db.transaction("image","readwrite");
+        let imageStore = imgTransaction.objectStore("image");
+        imageStore.delete(id);
+    }
+    e.target.parentElement.remove();
+}
+
+
+
+function dowloadListener(e){
+
+    let id = e.target.parentElement.getAttribute("id");
+    let type = id.slice(0,3);
+
+    if (type === "vid"){
+        let videoTransaction = db.transaction("video","readwrite");
+        let videoStore = videoTransaction.objectStore("video");
+        let videoRequest = videoStore.get(id);
+        videoRequest.onsuccess = (e) =>{
+            let videoResult = videoRequest.result;
+            let videoURL = URL.createObjectURL(videoResult.blobData);
+
+            let a = document.createElement("a");
+            a.href = videoURL;
+            a.download="stream.mp4";
+            a.click();
+        }
+    }
+    else if(type === "img"){
+        let imageTransaction = db.transaction("image","readwrite");
+        let imageStore = imageTransaction.objectStore("image");
+        let imageRequest = imageStore.get(id);
+        imageRequest.onsuccess = (e) =>{
+            let imageResult = imageRequest.result;
+            // let imageURL = URL.createObjectURL(imageResult.blobData);
+
+             let a = document.createElement("a");
+            a.href = imageResult.URL;
+            a.download="image.jpg";
+            a.click();
+        }
+    }
+}
 
 
     
